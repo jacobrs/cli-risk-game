0a1cd467d5d67973df02246ff5b40d11e586f84f
Darrin Fong <darrinfong@gmail.com>
2017-09-26 14:01:03 -0400

    Commented the way a .map file is organized

--- src/classes/MapLoader.cpp
+++ src/classes/MapLoader.cpp
@@ -5,6 +5,14 @@ MapLoader::MapLoader(string filePath){ /*File path is location of the game map t
 }
 
 bool MapLoader::importMap(){ /*Returns false if map is invalid (not in the standard .map format)*/
+		/*	Map format: 
+		*	[Continenets]
+			continent=capture points
+
+			[Territories]
+			name, IGNORE, IGNORE, continent, neighbours...
+		*/
+
 		
 	return false;
 }
