21c061338ece07352764c359922ec5bde7221b57
Darrin Fong <darrinfong@gmail.com>
2017-09-28 13:48:04 -0400

    added another case that creates a gameMap

--- src/mapLoaderMain.cpp
+++ src/mapLoaderMain.cpp
@@ -23,7 +23,7 @@ int main(int args, char** argv){
     MapLoader* mapLoader = new MapLoader("src/map/World.map");
     mapLoader->importMap();
 
-    printf("List all continents and their bonus value and countries:\n");
+    printf("\n\nLIST ALL CONTINENTS AND THEIR BONUS VALUES AND COUNTRIES\n");
     for(unsigned i = 0; i < mapLoader->mapContinents.size(); i++){
         Continent* tempCon = mapLoader->mapContinents[i];
         printf("\n%s", tempCon->name.c_str());
@@ -33,7 +33,7 @@ int main(int args, char** argv){
         }
     }
     
-    printf("List all countries and their neighbours:\n");
+    printf("\n\nLIST ALL COUNTRIES AND THEIR NEIGHBOURS:\n");
     for(unsigned i = 0; i < mapLoader->mapCountries.size(); i++){
         printf("\n%s", mapLoader->mapCountries[i]->name.c_str());
         for(unsigned j = 0; j < mapLoader->mapCountries[i]->numberOfNeighbours; j++){
@@ -44,22 +44,11 @@ int main(int args, char** argv){
     printf("\n\nEXPECTING CORRECT FILE PATH AND FILE FORMAT:\n");
     MapLoader* khorasLoader = new MapLoader("src/map/Khoras.map");
     khorasLoader->importMap();
+    GameMap* khoras = new GameMap();
 
-    printf("List all continents and their bonus value and countries:\n");
+    printf("CREATE GAMEMAP WITH IMPORTED FILE:\n");
     for(unsigned i = 0; i < khorasLoader->mapContinents.size(); i++){
         Continent* tempCon = khorasLoader->mapContinents[i];
-        printf("\n%s", tempCon->name.c_str());
-        printf("\t%i", tempCon->bonus);
-        for(unsigned j = 0; j < tempCon->numberOfCountries; j++){
-            printf("\t%s", tempCon->countries[j]->name.c_str());
-        }
-    }
-    
-    printf("List all countries and their neighbours:\n");
-    for(unsigned i = 0; i < khorasLoader->mapCountries.size(); i++){
-        printf("\n%s", khorasLoader->mapCountries[i]->name.c_str());
-        for(unsigned j = 0; j < khorasLoader->mapCountries[i]->numberOfNeighbours; j++){
-            printf("\t%s",  khorasLoader->mapCountries[i]->neighbours[j]->name.c_str());
-        }
+        khoras->addContinent(tempCon);
     }
 }
