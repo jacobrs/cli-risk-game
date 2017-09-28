cdd96a702c94f29582c8f0bd45a022efe1938ae8
Christina Nguyen <christina.thao.nguyen@hotmail.com>
2017-09-28 15:14:11 -0400

    Comments in driver
    make it easier to follow and fix some indentation

--- src/deckDriver.cpp
+++ src/deckDriver.cpp
@@ -5,16 +5,21 @@ int main (int agrs, char** argv){
 //Deck 
 Deck gameDeck = Deck(15);
 
+// A deck will has a vector to keep track of the cards and 
+// a second vector to keep track of the cards drawn from it
   gameDeck.printDeck();
   gameDeck.printDrawnPile();
+
+//Shuffle and redisplay to test function
   gameDeck.shuffleDeck();
   gameDeck.printDeck();
 
   Card drawn1 = gameDeck.draw();
   cout <<"\n Card drawn: "<< drawn1.printCard() <<endl;  
-
+//Print to see if top card got removed
   gameDeck.printDeck();     
 
+//Transfer card drawn to the hand
   Hand hand1 = Hand();
   hand1.addCard(drawn1);
 
@@ -29,11 +34,13 @@ Deck gameDeck = Deck(15);
   hand1.addCard(drawn3);
   hand1.addCard(drawn4);
   hand1.addCard(drawn5);
+
+//check cards in hand and the drawn pile
   hand1.printHand();
   gameDeck.printDrawnPile();
 
   gameDeck.printDeck();
-  
+// newArmy is the amount players get as a additiona army after exchanging their cards
   int newArmy =0;
 
   if (hand1.handSize >= 5){
@@ -62,6 +69,7 @@ Deck gameDeck = Deck(15);
   hand3.printHand();
   newArmy = hand3.exchange();
 
+// check that the bonus has increased
   cout << "Bonus armies from exchanging: " << newArmy << endl;
   
 
